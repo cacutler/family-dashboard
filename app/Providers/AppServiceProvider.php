1a6c7070a24ccaaf2224cc2889024019383e2b35
Alex Cutler <calexcutler@gmail.com>
2026-05-01 21:01:20 -0600

    Registered policies to data models in the app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,50 +1,40 @@
 <?php
-
 namespace App\Providers;
-
+use App\Models\ToDo;
+use App\Models\Event;
+use App\Models\User;
+use App\Policies\EventPolicy;
+use App\Policies\TodoPolicy;
+use App\Policies\UserPolicy;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
-
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
-    }
-
+    public function register(): void {}
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
+    public function boot(): void {
         $this->configureDefaults();
+        $this->configurePolicies();
     }
-
     /**
      * Configure default behaviors for production-ready applications.
      */
-    protected function configureDefaults(): void
-    {
+    protected function configureDefaults(): void {
         Date::use(CarbonImmutable::class);
-
-        DB::prohibitDestructiveCommands(
-            app()->isProduction(),
-        );
-
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
-                ->mixedCase()
-                ->letters()
-                ->numbers()
-                ->symbols()
-                ->uncompromised()
-            : null,
-        );
+        DB::prohibitDestructiveCommands(app()->isProduction());
+        Password::defaults(fn (): ?Password => app()->isProduction() ? Password::min(12)->mixedCase()->letters()->numbers()->symbols()->uncompromised() : null);
+    }
+    protected function configurePolicies(): void {
+        Gate::policy(User::class, UserPolicy::class);
+        Gate::policy(ToDo::class, TodoPolicy::class);
+        Gate::policy(Event::class, EventPolicy::class);
     }
 }
